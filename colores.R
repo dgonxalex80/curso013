12ea7d56f336cfd42d432ba21e91d80d80ce80af
Daniel E Gonzalez <dgonxalex80@gmail.com>
2026-07-27 00:42:24 -0500

    primera version

---
+++ colores.R
@@ -0,0 +1,64 @@
+# colores.R
+# Módulo 1 - Naranja
+# -------------------------
+c11 <- "#F6A623"   # Color principal
+c12 <- "#F7DFC1"   # Color de contraste
+# -------------------------
+# Módulo 2 - Coral
+# -------------------------
+c21 <- "#E85A67"
+c22 <- "#F8DCE0"
+# -------------------------
+# Módulo 3 - Azul
+# -------------------------
+c31 <- "#3498C9"
+c32 <- "#DCEFF8"
+# -------------------------
+# Módulo 4 - Azul petróleo
+# -------------------------
+c41 <- "#476885"
+c42 <- "#E2EAF1"
+# -------------------------
+# Módulo 5 - Verde menta
+# -------------------------
+c51 <- "#67C1A8"
+c52 <- "#DDF3EC"
+# -------------------------
+# Módulo 6 - Lavanda
+# -------------------------
+c61 <- "#8A6FD1"
+c62 <- "#EAE3F8"
+# -------------------------
+# Módulo 7 - Turquesa
+# -------------------------
+c71 <- "#29B8C8"
+c72 <- "#D9F3F6"
+# ==========================================================
+# Vectores útiles
+# ==========================================================
+
+# Colores principales
+paleta1 <- c(c11, c21, c31, c41, c51, c61, c71)
+# Colores de contraste
+paleta2 <- c(c12, c22, c32, c42, c52, c62, c72)
+# Paleta completa
+paleta <- c(
+  c11, c12,
+  c21, c22,
+  c31, c32,
+  c41, c42,
+  c51, c52,
+  c61, c62,
+  c71, c72
+)
+
+# Nombres de los módulos
+nombres_modulos <- c(
+  "Naranja",
+  "Coral",
+  "Azul",
+  "Azul petróleo",
+  "Verde menta",
+  "Lavanda",
+  "Turquesa"
+)
---
+++ init.R
@@ -0,0 +1,20 @@
+# colores base, claros y fuertes
+c11 <- "#F6A623"   # Color principal
+c12 <- "#F7DFC1"   # Color de contraste
+c21 <- "#E85A67"
+c22 <- "#F8DCE0"
+c31 <- "#3498C9"
+c32 <- "#DCEFF8"
+c41 <- "#476885"
+c42 <- "#E2EAF1"
+c51 <- "#67C1A8"
+c52 <- "#DDF3EC"
+c61 <- "#8A6FD1"
+c62 <- "#EAE3F8"
+c71 <- "#29B8C8"
+c72 <- "#D9F3F6"
+
+c7= "#2C5697"  # AZUL INSTITUCIONAL
+c8= "#F7F7F7"  # FONDO GRIS CLARO WEB
+
+
